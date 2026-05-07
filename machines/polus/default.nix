af4b98ce4754ac8feafb2e36c0ee40625f4078d6
Danilo Cianfrone <danilocianfr@gmail.com>
2026-05-07 13:43:45 +0200

    fix(darwin): reenabled homebrew modules

--- machines/polus/default.nix
+++ machines/polus/default.nix
@@ -17,7 +17,7 @@ darwin.lib.darwinSystem {
     ../../modules/system/fish.nix
     ../../modules/darwin/machine-default.nix
     ../../modules/darwin/aerospace.nix
-    # ./homebrew.nix # FIXME: https://github.com/Homebrew/brew/issues/22165
+    ./homebrew.nix
     ./docker.nix
     {
       system.primaryUser = "ar3s3ru";
